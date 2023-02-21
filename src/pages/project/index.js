642f88ff178e6d4d333fac0ca164c3ebc95ca12c
zahraa mazloum <zahraamazloum2001@gmail.com>
2023-02-21 13:19:15 +0200

    url

--- src/pages/project/index.js
+++ src/pages/project/index.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import './project.css'
 import Card from '../../components/card/card'
 import Navbar from '../../components/nav'
+
+const baseURL= "https://portfolio-backend-vert-tau.vercel.app/api/admin/"
 export class Project extends Component {
   componentDidMount() {
     document.title = "Project";
